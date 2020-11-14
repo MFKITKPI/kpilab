c46a6cafa9fc325673f54e03a79c71c83c96c49a
EdMorgun <MorgunEAu@gmail.com>
2020-11-14 16:02:30 +0200

    lab2 v1.1

---
+++ ConsoleApp1/EInformer/IExceptionInformer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApp1.EInformer
+{
+    public interface IExceptionInformer
+    {
+        public ushort GetErrors();
+        public bool Inform(Exception exception);
+    }
+}
